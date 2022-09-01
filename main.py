5bba64ff05b812d68cfee3a3d2c29508be13c015
Valentin387 <valentin.valencia@utp.edu.co>
2022-08-31 21:30:42 -0500

    some good advance

--- main.py
+++ main.py
@@ -4,14 +4,47 @@ class process:
         self.rafaga=rafaga
         self.time=time
 
-    def running_data(self,starTime,finishTime):
-        self.starTime=starTime
+    def set_running_data(self,startTime,finishTime):
+        self.startTime=startTime
         self.finishTime=finishTime
+        self.waitingTime=self.startTime-self.time
+        self.systemTime=self.finishTime-self.time
 
     def display(self):
         print(self.name , "\t", end="")
         print(self.rafaga , "\t", end="")
         print(self.time , "\t")
+
+    def display_start_finish(self):
+        print(self.name , "\t", end="")
+        print(self.rafaga , "\t", end="")
+        print(self.time , "\t", end="")
+        print(self.startTime , "\t", end="")
+        print(self.finishTime , "\t")
+
+def bubbleSort(arr):
+    n = len(arr)
+    # optimize code, so if the array is already sorted, it doesn't need
+    # to go through the entire process
+    swapped = False
+    # Traverse through all array elements
+    for i in range(n-1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j].time > arr[j + 1].time:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        if not swapped:
+            # if we haven't needed to make a single swap, we
+            # can just exit the main loop.
+            return
 
 
 if __name__ == "__main__":
@@ -19,7 +52,7 @@ if __name__ == "__main__":
     processList=[]
     halt=False
 
-    print("\t\t Welcome \n\n")
+    print("\t\t Welcome, this is FIFO dispatch algorithm \n\n")
     while(not halt):
         try:
             name=input("type process abreviated-name: ")
@@ -44,10 +77,49 @@ if __name__ == "__main__":
         if choice != 1:
             halt=True
 
-    print("\n\n")
     print("NAME\tRAFAGA\tTIME\t \n\n")
     for p in processList:
         p.display()
+
+    print("\n\n")
+    bubbleSort(processList)
+
+    print("\n\n Ordered by arrival time: \n")
+    print("NAME\tRAFAGA\tTIME\t \n\n")
+    for p in processList:
+        p.display()
+
+    tam=len(processList)
+    i=0
+    temp=0
+    rafagaAcumulada=0
+    while (i < tam):
+        while temp < i:
+            rafagaAcumulada+=processList[temp].rafaga
+            temp+=1
+        processList[i].set_running_data(rafagaAcumulada,rafagaAcumulada+p.rafaga)
+        rafagaAcumulada=0
+        temp=0
+        i+=1
+
+    print("\n\nNAME\tRAFAGA\tTIME\tstart\tfinish \n\n")
+    for p in processList:
+        p.display_start_finish()
+
+    tam=len(processList)
+    i=0
+    WT_total=0
+    ST_total=0
+    while (i < tam):
+        WT_total+=processList[i].waitingTime
+        ST_total+=processList[i].systemTime
+        i+=1
+    WT_average=WT_total/tam
+    ST_average=ST_total/tam
+
+    print("\n\n")
+    print("Waiting Time average: ",str(WT_average))
+    print("System Time average: ",str(ST_average))
 
 
     print("\n\n\nEND OF LINE")
